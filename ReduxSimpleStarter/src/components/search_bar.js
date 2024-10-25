9289954e0201613cb3bb777a2ae19553261ea708
bindhu <bindubaddam137@gmail.com>
2024-10-25 13:05:36 -0500

    new changes

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,39 +1,40 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 
 // const SearchBar = () => {
 //     return <input />
 // }
 // to add additional functionality like maintaining state we class component
 // class SearchBar extends React.Component{
-    class SearchBar extends Component{
-    
-        constructor(props){
-            super(props);
-            this.state = { 'term' : ""};
-        }
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
 
-        render(){
-            return (
-                <div className="search-bar" placeholder="search">
+  render() {
+    return (
+      <div className="search-bar" placeholder="search">
+        <input
+          value={this.state.term}
+          onChange={(event) => this.onInputChange(event.target.value)}
+        />
+        <br />
+      </div>
+    );
+  }
+  onInputChange(term) {
+    this.setState({ term });
+    this.props.onSearchTermChange(term);
+  }
 
-                    <input 
-                    value={this.state.term}
-                    onChange = {event => this.onInputChange(event.target.value)} />
-                    <br />
-           
-                 </div>
-            );
-        }
-        onInputChange(term)
-        {
-            this.setState({term});
-            this.props.onSearchTermChange(term);
-
-        }
-
-    // onChange(event){
-    //     console.log(event.target.value);
-    // }
+  // onChange(event){
+  //     console.log(event.target.value);
+  // }
 }
 
 export default SearchBar;
+// to add additional functionality like maintaining state we class component
+// class SearchBar extends React.Component{
+
+// to add additional functionality like maintaining state we class component
+// class SearchBar extends React.Component{
